37b7be4870159b6ec21e4235cf79ad314b717beb
lan-e <neladuranec@gmail.com>
2023-01-06 00:35:52 +0100

    sloziti Fotografija i dodati ostale radove

---
+++ script.js
@@ -0,0 +1,35 @@
+//Navigacija
+$(".fa-bars").click(function () {
+  $(".hamburgerNav").css("left", "0");
+  $("#navbar").hide();
+});
+
+$(".fa-xmark, .hamburgerNav ul a").click(function () {
+  $(".hamburgerNav").css("left", "-100%");
+  $("#navbar").show();
+});
+
+//Navbarscroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+//ScrollReveal
+
+const sr = ScrollReveal({
+  distance: "200px",
+  duration: 2500,
+  reset: true,
+});
+
+sr.reveal(".sr-top", { origin: "top" });
+sr.reveal(".sr-right", { origin: "right" });
+sr.reveal(".sr-bottom", { origin: "bottom" });
+sr.reveal(".sr-left", { origin: "left" });
