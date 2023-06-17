8173614ab1ebcfc495de0ab88063019901195e9e
lan-e <neladuranec@gmail.com>
2023-06-17 23:31:46 +0200

    optimization, styling, new projects

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ $(".fa-xmark, .hamburgerNav ul a").click(function () {
 });
 
 //Navbarscroll
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-90px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-90px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
 
 //ScrollReveal
 
@@ -44,3 +44,41 @@ $(document).ready(function () {
     nextImg();
   });
 });
+
+//ShowHideBtns
+const showHide = document.querySelector("#show-hide");
+const hideBtn = document.querySelector("#hide-btn");
+const showBtn = document.querySelector("#show-btn");
+function showMore() {
+  if (window.getComputedStyle(showHide).display === "none") {
+    showHide.style.display = "block";
+    hideBtn.style.display = "block";
+    showBtn.style.display = "none";
+  }
+}
+function showLess() {
+  if (window.getComputedStyle(showHide).display === "block") {
+    showHide.style.display = "none";
+    hideBtn.style.display = "none";
+    showBtn.style.display = "block";
+  }
+}
+
+//navbar on scroll change from transparent to solid
+const homeNav = document.querySelector(".homenav");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 850) {
+    homeNav.classList.add("navbar-scrolled");
+  } else if (window.scrollY < 850) {
+    homeNav.classList.remove("navbar-scrolled");
+  }
+});
+//navbar on hover change color
+function mouseOver() {
+  homeNav.classList.add("mouseover");
+}
+function mouseOut() {
+  homeNav.classList.remove("mouseover");
+}
+homeNav.addEventListener("mouseenter", mouseOver);
+homeNav.addEventListener("mouseleave", mouseOut);
